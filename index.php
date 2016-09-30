f0ceec9c88fc419e40fcb0ac8da4b7ec9bd00ed0
Rilind Pozhegu <Rilind Pozhegu>
2016-09-30 15:28:52 +0200

    y

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" type="text/css" href="css/font-awesome.css"> -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 
     <!-- Custom Fonts -->
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
